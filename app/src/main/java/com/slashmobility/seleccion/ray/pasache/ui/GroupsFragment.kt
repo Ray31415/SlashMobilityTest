4c7832c8b27a54c782d8ff74c2eec4492687787c
Ray Pasache <ruddy.pasache@gmail.com>
2022-11-26 13:19:55 +0100

    Handling navigation component

--- app/src/main/java/com/slashmobility/seleccion/ray/pasache/ui/GroupsFragment.kt
+++ app/src/main/java/com/slashmobility/seleccion/ray/pasache/ui/GroupsFragment.kt
@@ -1,6 +1,22 @@
 package com.slashmobility.seleccion.ray.pasache.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.slashmobility.seleccion.ray.pasache.databinding.FragmentGroupsBinding
 
 class GroupsFragment: Fragment() {
+
+    var binding: FragmentGroupsBinding?= null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentGroupsBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
 }
