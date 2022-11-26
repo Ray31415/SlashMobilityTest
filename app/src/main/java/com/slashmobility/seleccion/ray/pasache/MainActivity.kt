a93526a351e179fc4a8fd0daaaf70bbc5fd4924f
Ray Pasache <ruddy.pasache@gmail.com>
2022-11-26 11:51:43 +0100

    SlashMobility project test creation

---
+++ app/src/main/java/com/slashmobility/seleccion/ray/pasache/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.slashmobility.seleccion.ray.pasache
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
