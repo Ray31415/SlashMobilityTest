df90d1dcf069eb2ada5ad5fadda4a670db015f1d
Ray Pasache <ruddy.pasache@gmail.com>
2022-11-26 13:55:21 +0100

    handling group api model

--- app/src/main/java/com/slashmobility/seleccion/ray/pasache/model/GroupAPIModel.kt
+++ app/src/main/java/com/slashmobility/seleccion/ray/pasache/model/GroupAPIModel.kt
@@ -1,4 +1,12 @@
 package com.slashmobility.seleccion.ray.pasache.model
 
-class GroupAPIModel {
-}
+import com.google.gson.annotations.SerializedName
+
+class GroupAPIModel(
+    @SerializedName("date") var date: Long?,
+    @SerializedName("defaultImageUrl") var defaultImageUrl: String?,
+    @SerializedName("description") var description: String?,
+    @SerializedName("descriptionShort") var descriptionShort: String?,
+    @SerializedName("id") var id: Int?,
+    @SerializedName("name") var name: String?
+)
